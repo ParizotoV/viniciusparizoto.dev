ce2d3f9e053c4f954fc4c8a6d1dbcb13bab06ea2
Vinicius Parizoto <vinicius@clientbase.com.br>
2023-01-18 23:28:43 -0300

    Fix vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import React from 'react'
-import '../styles/Globals.css'
+import '../styles.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
